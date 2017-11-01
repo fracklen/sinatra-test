a9d3bdf8fc71ce6f5e18ed4691d9ddb4d496c4c8
Martin Neiiendam <fracklen@gmail.com>
2017-11-01 15:16:28 +0100

    Add append and delete actions

--- app.rb
+++ app.rb
@@ -8,12 +8,25 @@ get '/health' do
   "OK"
 end
 
-get '/load' do
+get '/append' do
   open('/data/file', 'a') do |f|
     1000.times do |i|
       1000.downto(1) do |j|
         f.puts("#{Math.sqrt(j) * i / 0.2}\n")
       end
+    end
+  end
+  [200, {}, "OK"]
+end
+
+delete '/remove' do
+  File.delete('/data/file')
+end
+
+get '/load' do
+  1000.times do |i|
+    1000.downto(1) do |j|
+      f.puts("#{Math.sqrt(j) * i / 0.2}\n")
     end
   end
   [200, {}, "OK"]
