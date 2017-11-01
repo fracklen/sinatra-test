d3fe49fe07d2ae14b4196536bf2919801649535e
Martin Neiiendam <fracklen@gmail.com>
2017-11-01 14:11:18 +0100

    Add load endpoint

--- app.rb
+++ app.rb
@@ -3,3 +3,16 @@ require 'sinatra'
 get '/' do
   "Just Do It"
 end
+
+get '/health' do
+  "OK"
+end
+
+get '/load' do
+  10000.times do |i|
+    1000.downto(1) do |j|
+      Math.sqrt(j) * i / 0.2
+    end
+  end
+  [200, {}, "OK"]
+end
